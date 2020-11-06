9482dc61707b80695b454ce49e28ad7c602f30a2
yazanHindi <yhindi918@gmail.com>
2020-11-07 01:21:56 +0200

    about school - news page

---
+++ SchoolWeb/Areas/Public/Controllers/AboutSchoolController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWeb.Areas.Public.Controllers
+{
+    [Area("Public")]
+    public class AboutSchoolController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AboutSchoolController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var ListOfActivities = _unitOfWork
+                            .Activity
+                            .GetAll(includeProperities: "ActivityImages")
+                            .ToList()
+                            .OrderBy(i => i.Date)
+                            .Take(3);
+
+            return View(ListOfActivities);
+        }
+    }
+}
