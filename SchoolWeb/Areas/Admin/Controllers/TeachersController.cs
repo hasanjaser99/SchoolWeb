3e1edd8ebd8e38eb0566b025df5336d69f7cad00
hasanjaser99 <hasan1999255@hotmail.com>
2020-11-09 00:34:03 +0200

    add GetAllTeachers Screen (admin-teachers)

---
+++ SchoolWeb/Areas/Admin/Controllers/TeachersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolWeb.DataAccess.Repository;
+
+namespace SchoolWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TeachersController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TeachersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //Get All Teachers
+        public IActionResult Index()
+        {
+            var teachers = _unitOfWork.Teacher.GetAll();
+
+            return View(teachers);
+        }
+
+    }
+}
