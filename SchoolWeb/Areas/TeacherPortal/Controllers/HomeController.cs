646b032af2967b1db3b24694fd1fd6982cf26354
yazanHindi <yhindi918@gmail.com>
2020-11-14 22:25:33 +0200

    teacherPortal-profile

---
+++ SchoolWeb/Areas/TeacherPortal/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SchoolWeb.DataAccess.Repository;
+
+namespace SchoolWeb.Areas.TeacherPortal.Controllers
+{
+    [Area("TeacherPortal")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = claims.Value;
+            var Teacher = _unitOfWork.Teacher.GetFirstOrDefault(t => t.Id == userId);
+            
+
+            return View(Teacher);
+        }
+    }
+}
